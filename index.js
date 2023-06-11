6231261319af830c56ea468101c15d618e4cc704
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-11 17:52:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let newHeader = document.createElement("h1");
+let y = document.querySelector('main#main')
+y.remove();
+newHeader.textContent = "Mohamed Yusuf is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
